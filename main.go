ec5a2b07e4b5c8e9f2862b1137b1f5ec36b06e9a
Alex S. Garzão <alexgarzao@gmail.com>
2024-02-16 22:37:16 -0300

    feat: deals with parameters and invokes jasm generation through the parser

--- main.go
+++ main.go
@@ -4,34 +4,34 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/alexgarzao/poj/codegen"
 	"github.com/alexgarzao/poj/parsing"
 	"github.com/antlr4-go/antlr/v4"
 )
 
-type TreeShapeListener struct {
-	*parsing.BasePascalListener
-}
-
-func NewTreeShapeListener() *TreeShapeListener {
-	return new(TreeShapeListener)
-}
-
-func (t *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
-}
-
 func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("POJ: Pascal on the JVM")
+		fmt.Println()
+		fmt.Println("Usage:")
+		fmt.Println("\tpoj <file>.pas")
+		os.Exit(1)
+	}
+
 	input, err := antlr.NewFileStream(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	fmt.Println("Compiling ", os.Args[1])
+
 	lexer := parsing.NewPascalLexer(input)
-	stream := antlr.NewCommonTokenStream(lexer, 0)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 	p := parsing.NewPascalParser(stream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	listener := codegen.NewTreeShapeListener()
+	antlr.ParseTreeWalkerDefault.Walk(listener, p.Program())
 
-	tree := p.Program()
-	antlr.ParseTreeWalkerDefault.Walk(NewTreeShapeListener(), tree)
+	fmt.Print(listener.Code())
 }
