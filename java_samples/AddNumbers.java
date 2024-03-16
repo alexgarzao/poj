e2921ea76dce8655df625f4377d49650411f2174
Alex S. Garzão <alexgarzao@gmail.com>
2024-03-16 18:29:36 -0300

    test: java samples

---
+++ java_samples/AddNumbers.java
@@ -0,0 +1,7 @@
+public class AddNumbers {
+   public static void main(String[] args) {
+      int v1 = 111;
+      int v2 = 222;
+      System.out.println(v1+v2);
+   }
+}
---
+++ java_samples/StringConcat.java
@@ -0,0 +1,9 @@
+public class StringConcat {
+   public static void main(String[] args) {
+
+       String myString1 = "Code";
+       String myString2 = "Gym";
+       String myString3 = myString1 + myString2;
+       System.out.println(myString3);
+   }
+}
