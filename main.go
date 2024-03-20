764f8a88203c1ade2896476439c4387fd828b8ae
Alex S. Garzão <alexgarzao@gmail.com>
2024-03-19 22:24:41 -0300

    refac: extract main to automate tests

--- main.go
+++ main.go
@@ -21,22 +21,45 @@ func main() {
 
 	inputFile := os.Args[1]
 
-	input, err := antlr.NewFileStream(inputFile + ".pas")
+	fmt.Println("Compiling ", inputFile)
+
+	jasm, err := genCode(inputFile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Compiling ", inputFile)
+	err = genFile(inputFile, jasm)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
 
-	lexer := parsing.NewPascalLexer(input)
+func genCode(inputFile string) (string, error) {
+	inputStream, err := antlr.NewFileStream(inputFile + ".pas")
+	if err != nil {
+		return "", err
+	}
+
+	filename := path.Base(inputFile)
+
+	lexer := parsing.NewPascalLexer(inputStream)
 	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 	p := parsing.NewPascalParser(stream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
 
-	filename := path.Base(inputFile)
 	listener := codegen.NewTreeShapeListener(filename)
 	antlr.ParseTreeWalkerDefault.Walk(listener, p.Program())
 
-	os.WriteFile(filename+".jasm", []byte(listener.Code()), 0666)
+	return listener.Code(), nil
+}
+
+func genFile(inputFile string, jasm string) error {
+	filename := path.Base(inputFile)
+	if err := os.WriteFile(filename+".jasm", []byte(jasm), 0666); err != nil {
+		return err
+	}
+
+	return nil
 }
