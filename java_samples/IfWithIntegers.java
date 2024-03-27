0263bd7eb74d46f52882e0d7bd5734217d01a485
Alex S. Garzão <alexgarzao@gmail.com>
2024-03-26 22:15:04 -0300

    test: java samples to understand how to generate "if" in jasm

---
+++ java_samples/IfWithIntegers.java
@@ -0,0 +1,10 @@
+public class IfWithIntegers {
+   public static void main(String[] args) {
+      int v1 = 111;
+      int v2 = 222;
+      if ( v1 > v2 )
+         System.out.println("v1>v2");
+      else
+         System.out.println("v1<=v2");
+   }
+}
