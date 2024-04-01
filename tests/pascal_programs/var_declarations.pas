b41ff500d1553fdf60e7bd13395317364386466a
Alex S. Garzão <alexgarzao@gmail.com>
2024-04-01 20:29:54 -0300

    test: pascal example with var declarations

---
+++ tests/pascal_programs/var_declarations.pas
@@ -0,0 +1,15 @@
+program Hello;
+var
+  msg1 : string;
+  msg2 : string;
+  msg3, msg4 : string;
+begin
+  msg1 := 'Message 1!';
+  msg2 := 'Message 2!';
+  msg3 := 'Message 3!';
+  msg4 := 'Message 4!';
+  writeln (msg1);
+  writeln (msg2);
+  writeln (msg3);
+  writeln (msg4);
+end.
