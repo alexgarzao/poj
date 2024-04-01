1b65c019df3f4cffde4802e9a7dc5d19aa3fb944
Alex S. Garzão <alexgarzao@gmail.com>
2024-03-31 21:27:00 -0300

    refact: conditional tests more verbose

--- tests/pascal_programs/if_with_integers_with_and_or.pas
+++ tests/pascal_programs/if_with_integers_with_and_or.pas
@@ -1,63 +1,63 @@
 program IfWithInteger;
 begin
   {* Simple conditional operator. *}
-  write('2>1? ');
+  write('2>1 must be true: ');
   if (2 > 1) then
     writeln('true')
   else
     writeln('false');
 
-  write('1>2? ');
+  write('1>2 must be false: ');
   if (1 > 2) then
     writeln('true')
   else
     writeln('false');
 
   {* AND operator. *}
-  write('111>222 and 222>333? ');
+  write('111>222 and 222>333 must be false: ');
   if ( 111 > 222) and ( 222 > 333 ) then
     writeln('true')
   else
     writeln('false');
 
-  write('111<222 and 222<333? ');
+  write('111<222 and 222<333 must be true: ');
   if ( 111 < 222) and ( 222 < 333 ) then
     writeln('true')
   else
     writeln('false');
 
-  write('111>222 and 222<333? ');
+  write('111>222 and 222<333 must be false: ');
   if ( 111 > 222) and ( 222 < 333 ) then
     writeln('true')
   else
     writeln('false');
 
-  write('111<222 and 222>333? ');
+  write('111<222 and 222>333 must be false: ');
   if ( 111 < 222) and ( 222 > 333 ) then
     writeln('true')
   else
     writeln('false');
   
   {* OR operator. *}
-  write('111>222 or 222>333? ');
+  write('111>222 or 222>333 must be false: ');
   if ( 111 > 222) or ( 222 > 333 ) then
     writeln('true')
   else
     writeln('false');
 
-  write('111<222 or 222<333? ');
+  write('111<222 or 222<333 must be true: ');
   if ( 111 < 222) or ( 222 < 333 ) then
     writeln('true')
   else
     writeln('false');
 
-  write('111>222 or 222<333? ');
+  write('111>222 or 222<333 must be true: ');
   if ( 111 > 222) or ( 222 < 333 ) then
     writeln('true')
   else
     writeln('false');
 
-  write('111<222 or 222>333? ');
+  write('111<222 or 222>333 must be true: ');
   if ( 111 < 222) or ( 222 > 333 ) then
     writeln('true')
   else
--- tests/pascal_programs/if_with_integers_without_and_or.pas
+++ tests/pascal_programs/if_with_integers_without_and_or.pas
@@ -1,111 +1,110 @@
 program SubIntegers;
 begin
   {* Check > if then sentence (else true). *}
-  write('111>222? ');
+  write('111>222  must be false: ');
   if ( 111 > 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check > if then sentence (then true). *}
-  write('333>222? ');
+  write('333>222 must be true: ');
   if ( 333 > 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check > if sentence (then false). *}
-  writeln('2>4? ');
+  writeln('2>4 must be false: ');
   if ( 2 > 4 ) then
     writeln('true');
 
   {* Check > if sentence (then true). *}
-  write('4>2? ');
+  write('4>2 must be true: ');
   if ( 4 > 2 ) then
     writeln('true');
 
   {* Check < if then sentence (then true). *}
-  write('111<222? ');
+  write('111<222 must be true: ');
   if ( 111 < 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check < if then sentence (else true). *}
-  write('222<111? ');
+  write('222<111 must be false: ');
   if ( 222 < 111 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check >= if then sentence (then true). *}
-  write('222>=111? ');
+  write('222>=111 must be true: ');
   if ( 222 >= 111 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check >= if then sentence (else true). *}
-  write('111>=222? ');
+  write('111>=222  must be false: ');
   if ( 111 >= 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check >= if then sentence (then true). *}
-  write('111>=111? ');
+  write('111>=111 must be true: ');
   if ( 111 >= 111 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check <= if then sentence (then true). *}
-  write('111<=222? ');
+  write('111<=222 must be true: ');
   if ( 111 <= 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check >= if then sentence (else true). *}
-  write('222<=111? ');
+  write('222<=111 must be false: ');
   if ( 222 <= 111 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check <= if then sentence (then true). *}
-  write('111<=111? ');
+  write('111<=111 must be true: ');
   if ( 111 <= 111 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check = if then sentence (then true). *}
-  write('222=222? ');
+  write('222=222 must be true: ');
   if ( 222 = 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check = if then sentence (else true). *}
-  write('111=222? ');
+  write('111=222 must be false: ');
   if ( 111 = 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check <> if then sentence (then true). *}
-  write('111<>222? ');
+  write('111<>222 must be true: ');
   if ( 111 <> 222 ) then
     writeln('true')
   else
     writeln('false');
 
   {* Check <> if then sentence (else true). *}
-  write('111<>111? ');
+  write('111<>111 must be false: ');
   if ( 111 <> 111 ) then
     writeln('true')
   else
     writeln('false');
-
 end.
