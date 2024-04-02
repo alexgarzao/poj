f76207b25445521e8aaa4e4913c08a22d036dd47
Alex S. Garzão <alexgarzao@gmail.com>
2024-04-02 00:17:48 -0300

    feat: downto in for statement

--- tests/pascal_programs/for.pas
+++ tests/pascal_programs/for.pas
@@ -4,11 +4,16 @@ var
 begin
   for i := 10 to 19 do
   begin
-    writeln ('i=', i);
+    writeln ('inc i=', i);
   end;
 
   for i := 10 to 2 do
   begin
     writeln ('dont write i=', i);
+  end;
+
+  for i := 10 downto 5 do
+  begin
+    writeln ('dec i=', i);
   end;
 end.
