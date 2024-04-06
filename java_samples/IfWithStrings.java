42c6b32be74368381b5e011b926fd7477732aba9
Alex S. Garzão <alexgarzao@gmail.com>
2024-04-06 17:46:35 -0300

    test: to understand how string operators works in the JVM

---
+++ java_samples/IfWithStrings.java
@@ -0,0 +1,10 @@
+public class IfWithStrings {
+   public static void main(String[] args) {
+      String v1 = "aaa";
+      String v2 = "bbb";
+      if (v1.compareTo(v2) > 0)
+         System.out.println("v1>v2");
+      else
+         System.out.println("v1<=v2");
+   }
+}
