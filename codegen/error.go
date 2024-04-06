26816073da6fffead78b371911da0fb5bda173b6
Alex S. Garzão <alexgarzao@gmail.com>
2024-04-05 21:02:28 -0300

    feat: tree parser now catch a lexer and parser error list

---
+++ codegen/error.go
@@ -0,0 +1,39 @@
+package codegen
+
+import "github.com/antlr4-go/antlr/v4"
+
+type CustomSyntaxError struct {
+	line, column int
+	msg          string
+}
+
+func (c *CustomSyntaxError) Error() string {
+	return c.msg
+}
+
+type CustomErrorListener struct {
+	*antlr.DefaultErrorListener // Embed default which ensures we fit the interface
+	Errors                      []error
+}
+
+func (c *CustomErrorListener) Add(err error) {
+	c.Errors = append(c.Errors, err)
+}
+
+func (c *CustomErrorListener) String() string {
+	var s string
+
+	for _, err := range c.Errors {
+		s += err.Error() + "\n"
+	}
+
+	return s
+}
+
+func (c *CustomErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	c.Errors = append(c.Errors, &CustomSyntaxError{
+		line:   line,
+		column: column,
+		msg:    msg,
+	})
+}
