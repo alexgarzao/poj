09ecc90a3a56f40a3c4e7283fae87bca7efe7dbe
Alex S. Garzão <alexgarzao@gmail.com>
2024-04-28 12:41:18 -0300

    feat: symbol table deals with procedure definitions

--- codegen/symbol_table_test.go
+++ codegen/symbol_table_test.go
@@ -36,3 +36,36 @@ func TestSymbolTable_AddVariable(t *testing.T) {
 	err = st.AddVariable("MyVar", codegen.Integer)
 	assert.NotNil(t, err)
 }
+
+func TestSymbolTable_AddProcedure(t *testing.T) {
+	st := codegen.NewSymbolTable()
+	ok, symbol := st.Get("xpto")
+	assert.Equal(t, false, ok)
+	assert.Equal(t, codegen.UndefinedSymbolType, symbol.SymbolType)
+	assert.Equal(t, codegen.Undefined, symbol.PascalType)
+	assert.Equal(t, -1, symbol.Index)
+
+	err := st.AddProcedure("myproc")
+	assert.Nil(t, err)
+
+	ok, symbol = st.Get("myproc")
+	assert.Equal(t, true, ok)
+	assert.Equal(t, codegen.Procedure, symbol.SymbolType)
+	assert.Equal(t, codegen.Undefined, symbol.PascalType)
+	assert.Equal(t, 0, symbol.Index)
+
+	ok, symbol = st.Get("MyProc")
+	assert.Equal(t, true, ok)
+	assert.Equal(t, codegen.Procedure, symbol.SymbolType)
+	assert.Equal(t, codegen.Undefined, symbol.PascalType)
+	assert.Equal(t, 0, symbol.Index)
+
+	err = st.AddProcedure("myproc")
+	assert.NotNil(t, err)
+
+	err = st.AddProcedure("MyProc")
+	assert.NotNil(t, err)
+
+	err = st.AddVariable("MyProc", codegen.Integer)
+	assert.NotNil(t, err)
+}
