58082456f186692389695ee7e52aef4e2d041ecb
Alex S. Garzão <alexgarzao@gmail.com>
2024-04-28 12:38:52 -0300

    test: java samples to understand how to generate procedure call

---
+++ java_samples/ProcedureCall.java
@@ -0,0 +1,10 @@
+public class ProcedureCall {
+   public static void main(String[] args) {
+      System.out.println("Hello from main!");
+      myMethod();
+   }
+
+   static void myMethod() {
+      System.out.println("Hello from myMethod!");
+   }
+}
