d88d392f760afaded46e961f72f7b50dc10f3351
Alex S. Garzão <alexgarzao@gmail.com>
2024-05-03 20:38:12 -0300

    tests: pascal tests (global and local vars) and the expected jasm

---
+++ tests/valid_pascal_programs/global_and_local_vars_4.pas
@@ -0,0 +1,51 @@
+program global_and_local_vars_4;
+
+var
+    myglobal1: integer;
+    myglobal2: integer;
+    myglobal3: integer;
+    addedValue: integer;
+
+function addvalues(value1, value2: integer) : integer;
+var
+    mylocal1: integer;
+    mylocal2: integer;
+begin
+    mylocal1 := value1 + value2;
+    mylocal2 := mylocal1 + addedValue;
+    addvalues := mylocal2;
+end;
+
+function mulvalues(value1, value3: integer) : integer;
+var
+    mylocal1: integer;
+    mylocal3: integer;
+begin
+    mylocal1 := value1 * value3;
+    mylocal3 := mylocal1 + addedValue;
+    mulvalues := mylocal3;
+end;
+
+function subvalues(value1, value2: integer) : integer;
+var
+    mylocal1: integer;
+    mylocal2: integer;
+    addedValue: integer;
+begin
+    addedValue := 10;
+    mylocal1 := value1 - value2;
+    mylocal2 := mylocal1 + addedValue;
+    subvalues := mylocal2;
+end;
+
+begin
+    addedValue := 5;
+    myglobal1 := addvalues(2, 4);
+    myglobal2 := mulvalues(6, 8);
+    myglobal3 := subvalues(8, 4);
+
+    writeln('addedValue=', addedValue);
+    writeln('2+4 + addedValue=', myglobal1);
+    writeln('6*8 + addedValue=', myglobal2);
+    writeln('8-4 + local addedValue(10)=', myglobal3);
+end.
