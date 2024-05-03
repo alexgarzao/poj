baa1106052f7b181c59933117c64dcb1e9a9fcd6
Alex S. Garzão <alexgarzao@gmail.com>
2024-05-03 19:53:27 -0300

    test: java samples to understand how to use global variables

---
+++ java_samples/GlobalAndLocalVars.java
@@ -0,0 +1,8 @@
+public class GlobalAndLocalVars {
+   public static int globalVar;
+
+   public static void main(String[] args) {
+      globalVar = 123;
+      System.out.println(globalVar);
+   }
+}
